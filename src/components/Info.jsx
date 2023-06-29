5cddcd0ad5ac25f0c34feb9120ecacb5485b139b
Summer <swatson9008@yahoo.com>
2023-06-29 13:44:05 -0400

    working on code

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,15 +1,21 @@
+import cat from '../assets/cat.JPEG'
+import mail from '../assets/mail.png'
+import linkedin from '../assets/linkedin.png'
+
 export default function Info(){
     return (
         <div className="InfoBox">
             <div className="Photo">
-
+                <img src={cat} alt="profile"/>
             </div>
+            <div className='infoCenter'>
             <div className="Name">
-            Summer Watson
+                <h2>Summer Watson</h2>
             </div>
-            <div className="Title">Front End Web Developer</div>
-            <div>
-                <button className="Email">Email</button><button className="LinkedIn">LinkedIn</button>
+            <div className="Title">Frontend Web Developer</div>
+            <div className='buttonSpace'>
+                <button className="Email"><img src={mail} alt=""/>Email</button><button className="LinkedIn"><img src={linkedin} alt=""/>LinkedIn</button>
+            </div>
             </div>
         </div>
     )
